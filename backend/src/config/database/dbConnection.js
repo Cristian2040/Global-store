9738c8c0924b1342f2fd2b71f380a80f74aea404
FelipeEB <felipeestrellabautista@gmail.com>
2026-02-07 03:18:45 -0600

    fix: correct environment config import path in dbConnection

--- backend/src/config/database/dbConnection.js
+++ backend/src/config/database/dbConnection.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const { MONGO_URI } = require("./env");
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../environment/env');
 
 async function connectDB() {
   if (!MONGO_URI) throw new Error("MONGO_URI no configurado");
