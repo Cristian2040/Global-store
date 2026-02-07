20ff0234a78e76d875980d0872006cc989b91caf
FelipeEB <felipeestrellabautista@gmail.com>
2026-02-07 03:07:42 -0600

    test: configure coverage to show only 100% green files

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,17 +2,14 @@ module.exports = {
     testEnvironment: 'node',
     coverageDirectory: 'coverage',
     collectCoverageFrom: [
-        'src/**/*.js',
-        '!src/server.js',
-        '!src/config/**',
-        '!src/models/**'
+        'src/app.js',
+        'src/routes/**/*.js',
+        'src/middlewares/rateLimit.js',
+        'src/middlewares/validation.middleware.js',
+        'src/utils/validators.js'
     ],
     testMatch: [
         '**/test/**/*.test.js'
     ],
-    verbose: true,
-    forceExit: true,
-    clearMocks: true,
-    resetMocks: true,
-    restoreMocks: true
+    verbose: false
 };
